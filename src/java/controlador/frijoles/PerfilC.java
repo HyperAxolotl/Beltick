91bec675dbddd72c2821627ac1646b825979f919
Pernath <cgah.95@gmail.com>
2016-04-17 17:22:52 -0500

    Explorar Perfiles Esbozo del funcionamiento de caso de uso. Ya es posible acceder al conjunto de datos del perfil.

---
+++ src/java/controlador/frijoles/PerfilC.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador.frijoles;
+
+import controlador.logica.PerfilL;
+import modelo.PerfilPasajero;
+import modelo.PerfilChofer;
+import java.util.List;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author hyperaxolotl
+ */
+@ManagedBean
+@RequestScoped
+public class PerfilC {
+    private List<PerfilPasajero> pasajeros;
+    private List<PerfilChofer> choferes;
+    private final PerfilL ayudante = new PerfilL();
+    /**
+     * Creates a new instance of PerfilC
+     */
+    public PerfilC() {
+    }
+
+    public List<PerfilChofer> getChoferes() {
+        return choferes;
+    }
+
+    public List<PerfilPasajero> getPasajeros() {
+        return pasajeros;
+    }
+
+    public void setChoferes(List<PerfilChofer> choferes) {
+        this.choferes = choferes;
+    }
+
+    public void setPasajeros(List<PerfilPasajero> pasajeros) {
+        this.pasajeros = pasajeros;
+    }
+    
+    public void listarChoferes(){
+        choferes = ayudante.listaChoferes();
+    }
+    
+    public void listarPasajeros(){
+        pasajeros = ayudante.listaPasajeros();
+    }
+    
+    
+    
+    
+}
---
+++ src/java/controlador/logica/PerfilL.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador.logica;
+
+import java.util.List;
+import modelo.ConexionBD;
+import modelo.PerfilChofer;
+import modelo.PerfilPasajero;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+
+/**
+ *
+ * @author hyperaxolotl
+ */
+public class PerfilL {
+    private Session con;
+    
+    public List<PerfilPasajero> listaPasajeros() {
+        Criteria c;
+        List<PerfilPasajero> lstPasajeros = null;
+        try{
+            con = ConexionBD.getSessionFactory().openSession();
+            c = con.createCriteria(PerfilPasajero.class);
+            c.addOrder(Order.asc("idPpasajero"));
+            lstPasajeros = c.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //con.close();
+            return lstPasajeros;
+        }
+    }
+    
+    public List<PerfilChofer> listaChoferes() {
+        Criteria c;
+        List<PerfilChofer> lstChoferes = null;
+        try{
+            con = ConexionBD.getSessionFactory().openSession();
+            c = con.createCriteria(PerfilChofer.class);
+            c.addOrder(Order.asc("idPchofer"));
+            lstChoferes = c.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //con.close();
+            return lstChoferes;
+        }
+        
+        
+    }
+    
+    
+    
+    
+}
