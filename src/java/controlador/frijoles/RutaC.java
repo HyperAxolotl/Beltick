2409cf2dbd6d0c674c3379403c1a871b056f2927
andreagonz <andreagonz@ciencias.unam.mx>
2016-04-20 22:59:06 -0500

    Rutas
    Ya se muestran las rutas en el explorador, mapa y todo

--- src/java/controlador/frijoles/RutaC.java
+++ src/java/controlador/frijoles/RutaC.java
@@ -6,9 +6,16 @@ import java.io.Serializable;
 
 import modelo.Ruta;
 import controlador.logica.RutaL;
+import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
 import javax.servlet.http.HttpServletRequest;
+import org.primefaces.model.map.DefaultMapModel;
+import org.primefaces.model.map.LatLng;
+import org.primefaces.model.map.MapModel;
+import org.primefaces.model.map.Polyline;
+
 
 @ManagedBean
 @ViewScoped
@@ -17,8 +24,10 @@ public class RutaC {
     private Ruta ruta = new Ruta();
     private RutaL rutaL = new RutaL();
     private List<Ruta> lstRutas;
+    private MapModel modeloMapa  = new DefaultMapModel();;
+   
     
-    public List<Ruta> getLstRutas() {
+    public List<Ruta> getLstRutas() {       
         return lstRutas;
     }
     
@@ -26,4 +35,51 @@ public class RutaC {
         lstRutas = rutaL.listar();
     }
     
+    public MapModel getModeloMapa(String polyencod) {  
+        Polyline poly = new Polyline();
+        List<LatLng> l = decodePolyline(polyencod);
+        for(LatLng coord : l)
+            poly.getPaths().add(coord);
+        poly.setStrokeWeight(10);
+        poly.setStrokeColor("#FF9900");
+        poly.setStrokeOpacity(0.7);
+        
+        modeloMapa.addOverlay(poly);
+        return modeloMapa;
+    }
+    
+    
+    private List<LatLng> decodePolyline(String encoded) {
+ 
+        List<LatLng> poly = new ArrayList<>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+ 
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+ 
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+ 
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                        (((double) lng / 1E5)));
+            poly.add(p);
+        }
+ 
+        return poly;
+    }
 }
