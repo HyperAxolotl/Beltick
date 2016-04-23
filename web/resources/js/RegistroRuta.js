d92b71feb254226ff5b75c331507e54be2f957d0
andreagonz <andreagonz@ciencias.unam.mx>
2016-04-23 17:13:45 -0500

    Mejoras a registro de ruta
    Ya se puede borrar el ultimo punto ingredaso a demas de toda la ruta

--- web/resources/js/RegistroRuta.js
+++ web/resources/js/RegistroRuta.js
@@ -1,23 +1,41 @@
-var poly;
-
-function initMap() {
-    poly = new google.maps.Polyline({
-        strokeColor: '#000000',
-        strokeOpacity: 1.0,
-        strokeWeight: 3
-    });
-}
+var poly = new google.maps.Polyline({
+    strokeColor: '#DC143C',
+    strokeOpacity: 1.0,
+    strokeWeight: 3
+});
+//var mark;
 
 function addLatLng(event) {
     var path = poly.getPath();
-
     path.push(event.latLng);
-
-    var marker = new google.maps.Marker({
+   /* var marker = new google.maps.Marker({
         position: event.latLng,
-        title: '#' + path.getLength(),
+        title: '' + path.getLength(),
     });
-    PF('map').addOverlay(marker);
+    marker.setDraggable(true);
+    marker.addListener('dragend', actualizarPoly);
+    mark = marker;
+    PF('map').addOverlay(marker);*/
     PF('map').addOverlay(poly);
+    document.getElementById('mapa').value = google.maps.geometry.encoding.encodePath(poly.getPath());
+}
+
+/*function actualizarPoly() {
+    var path = poly.getPath();
+    var latlng = path.getAt(parseInt(mark.getLabel()) - 1);
+    latlng = mark.getPosition();
+    PF('map').addOverlay(poly);
+    document.getElementById('mapa').value = google.maps.geometry.encoding.encodePath(poly.getPath());
+}*/
+
+function borrarUltimo() {
+    var path = poly.getPath();
+    path.pop();
+    document.getElementById('mapa').value = google.maps.geometry.encoding.encodePath(poly.getPath());
+}
+
+function borrarTodo() {
+    var path = poly.getPath();
+    path.clear();
     document.getElementById('mapa').value = google.maps.geometry.encoding.encodePath(poly.getPath());
 }
