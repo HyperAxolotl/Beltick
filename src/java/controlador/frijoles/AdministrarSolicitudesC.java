4b4f2149361611967ba576be1f0c9feb510235b4
andreagonz <andreagonz@ciencias.unam.mx>
2016-05-03 05:48:54 -0500

    Administras solicitudes
    Los pasajeros ya pueden administrar solicitudes, i.e. aceptar o rechazar solicitudes. Pagina de 404 agregada

---
+++ src/java/controlador/frijoles/AdministrarSolicitudesC.java
@@ -0,0 +1,72 @@
+package controlador.frijoles;
+
+import controlador.logica.SolicitudL;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import modelo.Chofer;
+import modelo.Solicitud;
+
+@ManagedBean
+@ViewScoped
+public class AdministrarSolicitudesC implements Serializable {
+
+    private Solicitud solicitud;
+    private SolicitudL ayudante;
+    private FacesMessage mensaje;
+    private List<Solicitud> lstSolicitudes;
+    
+    public AdministrarSolicitudesC() {
+        solicitud = new Solicitud();
+        ayudante = new SolicitudL();
+    }
+
+    public Solicitud getSolicitud() {
+        return solicitud;
+    }
+
+    public void setSolicitud(Solicitud solicitud) {
+        this.solicitud = solicitud;
+    }
+
+    public List<Solicitud> getLstSolicitudes() {
+        return lstSolicitudes;
+    }
+
+    public void setLstSolicitudes(List<Solicitud> lstSolicitudes) {
+        this.lstSolicitudes = lstSolicitudes;
+    }
+
+    public void eliminarSolicitud(Solicitud s) throws Exception {
+        mensaje = ayudante.eliminar(s);
+        if (mensaje != null) {
+            FacesContext.getCurrentInstance().addMessage(null, mensaje);
+        }
+    }
+
+    public void registrarSolicitud(Solicitud s) throws Exception {
+        mensaje = ayudante.registrar(s);
+        if (mensaje != null) {
+            FacesContext.getCurrentInstance().addMessage(null, mensaje);
+        }
+    }
+
+    public void listarSolicitudes(Chofer chofer) throws Exception {
+        lstSolicitudes = ayudante.listar();
+        List<Solicitud> l = new ArrayList<>();
+        for (Solicitud s : lstSolicitudes)
+            if (s.getRuta().getAutomovil().getChofer().getIdChofer() == chofer.getIdChofer())
+                l.add(s);
+        lstSolicitudes = l;
+    }
+    
+    public String mapDia(int dia) {
+        String[] dias = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"};
+        return dias[dia - 1];
+    }
+    
+}
