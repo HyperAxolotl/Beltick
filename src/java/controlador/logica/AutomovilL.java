42a8d0daac7cd5cf2747db920458f97a10b993df
andreagonz <andreagonz@ciencias.unam.mx>
2016-05-25 23:57:55 -0500

    Cambio a registro automovil Ya no estan en paginas separadas el registro de automovil y ruta, ahora estan en la misma y se separan en tabs por el wizard de primefaces, esto es para facilitar la congruencia de nuestra aplicacion, que no se pueda registrar un auto sin registrar tambien una ruta

--- src/java/controlador/logica/AutomovilL.java
+++ src/java/controlador/logica/AutomovilL.java
@@ -1,12 +1,15 @@
 package controlador.logica;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import org.hibernate.Session;
 import modelo.ConexionBD;
 import modelo.Automovil;
 import modelo.Chofer;
 import modelo.Horario;
+import modelo.Ruta;
 import org.hibernate.Query;
 import org.hibernate.Transaction;
 
@@ -15,19 +18,31 @@ public class AutomovilL implements Serializable{
     private Session con;
     private Transaction trans;
     
-    public FacesMessage registrar(Automovil a, Chofer c){
+    public FacesMessage registrar(Automovil a, Ruta r, Horario h, Chofer c){
         FacesMessage mensaje = null;
         try{
             if (con == null || !con.isOpen())
                 con = ConexionBD.getSessionFactory().openSession();
             trans = con.beginTransaction();
             a.setPlaca(a.getPlaca().toUpperCase());
+            a.setChofer(c);
             c.getAutomovils().add(a);
             con.save(a);
+            Date fecha = new Date();
+            String mapa = FacesContext.getCurrentInstance().
+                    getExternalContext().getRequestParameterMap().get("mapa");
+            r.setMapa(mapa);
+            r.setActiva(true);
+            r.setFechaCreacion(fecha);
+            r.setAutomovil(a);
+            con.save(r);
+            h.setRuta(r);
+            con.save(h);
             trans.commit();
         }catch(Exception e){
             trans.rollback();
-            mensaje = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error con el registro del automovil", null);
+            c.getAutomovils().clear();
+            mensaje = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error al registrar los datos", null);
             e.printStackTrace();
         }finally{
             con.close();
@@ -35,20 +50,6 @@ public class AutomovilL implements Serializable{
         }
      
     }
-    
-//    public Automovil getAutomovil(int id) {
-//        Automovil a = null;
-//        try{
-//            if (con == null || !con.isOpen())
-//                con = ConexionBD.getSessionFactory().openSession();
-//            a = (Automovil)con.get(Automovil.class,id);
-//        }catch(Exception e){
-//            e.printStackTrace();
-//        }finally{
-//            con.close();
-//            return a;
-//        }
-//    }
     
     public Automovil getAutomovil(int choferId) {
         Automovil a = null;
